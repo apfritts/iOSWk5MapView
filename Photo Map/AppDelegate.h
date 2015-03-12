fd7735629c9defba23a543cd8025cf1bb6d44800
AP Fritts <ap@apfritts.com>
2015-03-11 19:05:00 -0700

    Initial Commit

---
+++ Photo Map/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Photo Map
+//
+//  Created by AP Fritts on 3/11/15.
+//  Copyright (c) 2015 Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Photo Map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photo Map
+//
+//  Created by AP Fritts on 3/11/15.
+//  Copyright (c) 2015 Box. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
